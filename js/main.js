ce58ea72f130b0531dff349bb949861a401b6c31
Amirreza Jamshidi <amir010101developer@gmail.com>
2024-05-09 18:05:09 +0330

    Completion of courses

--- js/main.js
+++ js/main.js
@@ -31,3 +31,20 @@ $('#multi_slider').owlCarousel({
         }
     }
 })
+
+$('#course_slider').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:4
+        }
+    }
+})
